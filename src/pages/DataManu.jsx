1d27b78094326870b6d0e05bb549d80aaee64557
Naomii01 <naomi222001220@gmail.com>
2024-05-03 11:21:21 +0200

    filter dropdown

--- src/pages/DataManu.jsx
+++ src/pages/DataManu.jsx
@@ -3,11 +3,15 @@ import { useState } from "react";
 import { data } from "../users";
 import "./DataManu.css";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
 function DataManu() {
   const [users, setUsers] = useState(data);
   const [sorted, setSorted] = useState({ sorted: "id", reversed: false });
   const [searchPhrase, setSearchPhrase] = useState("");
+  const [filterOption, setFilterOption] = useState("");
 
   const sortById = () => {
     setSorted({ sorted: "id", reversed: !sorted.reversed });
@@ -39,19 +43,25 @@ function DataManu() {
   };
 
   const search = (event) => {
-    const matchedUsers = data.filter((user)=>{
-    return `${user.first_name} ${user.last_name}`
-      .toLowerCase()
-      .includes(event.target.value.toLowerCase());
+    const matchedUsers = data.filter((user) => {
+      return `${user.first_name} ${user.last_name}`
+        .toLowerCase()
+        .includes(event.target.value.toLowerCase());
     });
     setUsers(matchedUsers);
-    setSearchPhrase(event.target.value)
-  }
+    setSearchPhrase(event.target.value);
+  };
+
+  const filterByGender = (event) => {
+    const filteredUsers = data.filter((user) => user.gender === event.target.value);
+    setUsers(filteredUsers);
+    setFilterOption(event.target.value);
+  };
 
   const renderUsers = () => {
     return users.map((user) => {
       return (
-        <tr>
+        <tr key={user.id}>
           <td>{user.id}</td>
           <td>{`${user.first_name} ${user.last_name}`}</td>
           <td>{user.email}</td>
@@ -71,9 +81,26 @@ function DataManu() {
   return (
     <div className="DataManu">
       <div className="search-container">
-        <input type="text"
-         placeholder="Search" 
-         value={searchPhrase} onChange={search}/>
+        <TextField
+          label="Search"
+          variant="outlined"
+          value={searchPhrase}
+          onChange={search}
+        
+        />
+        <Select
+          value={filterOption}
+          onChange={filterByGender}
+          displayEmpty
+          inputProps={{ "aria-label": "Filter by gender" }}
+        >
+          <MenuItem value="" disabled>
+            Filter by Gender
+          </MenuItem>
+          <MenuItem value="Male">Male</MenuItem>
+          <MenuItem value="Female">Female</MenuItem>
+          <MenuItem value="Other">Other</MenuItem>
+        </Select>
       </div>
       <div className="table-container">
         <table>
